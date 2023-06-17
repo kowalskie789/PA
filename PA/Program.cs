7334eb66e33b348c72c63906f7c96be87ebccf67
kowalskie789@gma <kowalskie789@gmail.com>
2023-06-17 09:23:04 +0200

    Add project files.

---
+++ PA/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+class Program
+{
+    static void Main(string[] args)
+    {
+        Console.WriteLine("Hello, world!");
+
+        // Tutaj możesz dodać swoją logikę i kod programu
+
+        Console.ReadLine(); // Oczekiwanie na naciśnięcie klawisza Enter przed zakończeniem programu
+    }
+}
