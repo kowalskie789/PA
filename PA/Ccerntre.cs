3c7534231ba10d2646a1cc74775b0008e57b5964
kowalskie789@gma <kowalskie789@gmail.com>
2023-06-17 10:45:55 +0200

    Powinno działać :/

---
+++ PA/Ccerntre.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Concurrent;
+
+// Creating employees
+Agent agent1 = new Agent();
+agent1.Id = 1;
+agent1.Name = "Agent 1";
+agent1.SeniorityLevel = 1;
+
+Agent agent2 = new Agent { Id = 2, Name = "Agent 2", SeniorityLevel = 2 };
+Agent agent3 = new Agent { Id = 3, Name = "Agent 3", SeniorityLevel = 3 };
+Employee manager1 = new Employee { Id = 4, Name = "Manager 1" };
+
+// Creating queues
+Queue highPriorityQueue = new Queue();
+highPriorityQueue.Name = "High Prriority";
+highPriorityQueue.Agents = new List<Agent>();
+highPriorityQueue.WaitingCalls = new List<Call>();
+
+highPriorityQueue.Agents.Add(agent1);
+
+Queue normalQueue = new Queue();
+normalQueue.Name = "Normal Priority";
+normalQueue.Agents = new List<Agent>();
+normalQueue.WaitingCalls = new List<Call>();
+normalQueue.Agents.Add(agent1);
+normalQueue.Agents.Add(agent2);
+normalQueue.Agents.Add(agent3);
+
+// Creating calls
+var call1 = new Call { Time = DateTime.Now, Accepted = true, Agent = agent1 };
+var call2 = new Call { Time = DateTime.Now.AddMinutes(-10), Accepted = true, Agent = agent2 };
+var call3 = new Call { Time = DateTime.Now.AddMinutes(-5), Accepted = false };
+
+// Adding calls to queues
+highPriorityQueue.WaitingCalls.Add(call1);
+normalQueue.WaitingCalls.Add(call2);
+normalQueue.WaitingCalls.Add(call3);
+
+// Creating call center
+CallCentre callCentre = new CallCentre();
+callCentre.Employees = new List<Employee>();
+callCentre.Queues = new List<Queue>();
+callCentre.Employees.Add(agent1);
+callCentre.Employees.Add(agent2);
+callCentre.Employees.Add(agent3);
+callCentre.Employees.Add(manager1);
+callCentre.Queues.Add(highPriorityQueue);
+callCentre.Queues.Add(normalQueue);
+
+Console.WriteLine("Longest waiting call:");
+
+DateTime date1 = DateTime.Now;
+DateTime date2 = normalQueue.GetLongestWaitingCall().Time;
+TimeSpan difference = date1 - date2;
+Console.WriteLine(difference.Minutes + " Minutes");
+
+        
+     
--- PA/Program.cs
+++ PA/Program.cs
@@ -1,13 +1,39 @@
 ﻿using System;
 
-class Program
+ class Employee
 {
-    static void Main(string[] args)
+    public int Id { get; set; }
+    public string Name { get; set; }
+}
+class Agent : Employee
+{
+    public int SeniorityLevel { get; set; }
+}
+
+class Call
+{
+    public DateTime Time { get; set; }
+    public bool Accepted { get; set; }
+    public Employee? Agent { get; set; }
+}
+
+class Queue
+{
+    public string Name { get; set; }
+    public List<Call>? WaitingCalls { get; set; }
+    public List<Agent>? Agents { get; set; }
+
+    public Call GetLongestWaitingCall()
     {
-        Console.WriteLine("Hello, world!");
 
-        // Tutaj możesz dodać swoją logikę i kod programu
-
-        Console.ReadLine(); // Oczekiwanie na naciśnięcie klawisza Enter przed zakończeniem programu
+        return WaitingCalls.OrderBy(call => call.Time).FirstOrDefault();
     }
+}
+
+class CallCentre
+{
+    public List<Employee> Employees { get; set; }
+    public List<Queue> Queues { get; set; }
+
+   
 }
